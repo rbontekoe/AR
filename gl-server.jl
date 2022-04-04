c5cc1813cb0e494ce88b18e2acd3f693387a384e
rbontekoe <rbontekoe@appligate.nl>
2022-04-04 15:01:41 +0200

    test gl

--- gl-server.jl
+++ gl-server.jl
@@ -6,7 +6,11 @@ using Sockets, Serialization, AppliGeneralLedger
         sock = accept(server)
         @async while isopen(sock)
             y = deserialize(sock)
-            AppliGeneralLedger.process(y; path_journal="/var/lib/postgresql/data/journal.txt", path_ledger="/var/lib/postgresql/data/generalledger.txt")
+            if (y isa String && y == "gl")
+                write(sock, serialize(sock, AppliGeneralLedger.AppliGeneralLedger.read_from_file("/var/lib/postgresql/data/generalledger.txt")))
+            else
+                AppliGeneralLedger.process(y; path_journal="/var/lib/postgresql/data/journal.txt", path_ledger="/var/lib/postgresql/data/generalledger.txt")
+            end
         end
     end
 end
